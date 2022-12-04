5bd1ed05164783a36646f7745ae8de37c420e592
Daniel Ward <02danwar.buss@gmail.com>
2022-12-04 16:42:08 -0600

    Header main section complete

--- src/components/HeaderSectionF/BookRegistration.jsx
+++ src/components/HeaderSectionF/BookRegistration.jsx
@@ -2,7 +2,7 @@ function BookRegistration() {
   return (
     <>
       <div>
-        <div className="bg-[#ff4157] w-full md:w-[80%] mx-auto relative md:bottom-24 h-44 p-10"></div>
+        <div className="bg-[#ff4157] w-full md:w-[80%] mx-auto bottom-5 relative md:bottom-24 h-44 p-10"></div>
       </div>
     </>
   );
--- src/components/HeaderSectionF/HeaderSection.jsx
+++ src/components/HeaderSectionF/HeaderSection.jsx
@@ -1,18 +1,93 @@
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import BookRegistration from "./BookRegistration";
 
 function HeaderSection() {
+  const [slideContainerHover, setSlideContainerHover] = useState(false);
+  const slideContainer = useRef(0);
+
+  const draggableSlide = () => {
+    slideContainer.current.addEventListener(
+      "mousemove",
+      (e) => {
+        let breakPoint = 750;
+        // console.log(e.pageX);
+
+        if (e.pageX <= breakPoint) {
+          slideContainer.current.addEventListener("mousedown", () => {
+            slideContainer.current.scrollBy(-1000, 0);
+          });
+        } else if (e.pageX > breakPoint) {
+          slideContainer.current.addEventListener("mousedown", () => {
+            slideContainer.current.scrollBy(1000, 0);
+          });
+        }
+      },
+      1
+    );
+  };
+
+  useEffect(() => {
+    if (window.innerWidth <= 1620) {
+      return console.log("Mobile/Tablet/Laptop");
+    } else {
+      draggableSlide();
+    }
+  });
+
   return (
     <>
-      <div className="active:cursor-grabbing">
-        <div className="bg-[url(https://ld-wp73.template-help.com/wordpress/prod_803/v1/wp-content/uploads/2020/03/slider_picture2.jpg)] w-full h-[670px] bg-cover bg-center bg-no-repeat flex justify-center items-center flex-col text-white active:cursor-grabbing text-center">
-          <h3 className="text-xl font-[Times-new-romen] tracking-[10px]">
-            COMFORTABLE APARTMENTS
-          </h3>
-          <h1 className="header-title text-5xl px-5 sm:text-8xl">
-            ENJOY YOUR STAY WHILE ON A BUSINESS TRIP
-          </h1>
+      <div>
+        <div
+          className={`absolute duration-300 arrow-left left-10 sm:left-32 top-96 sm:top-64 cursor-pointer ${
+            slideContainerHover ? "opacity-100" : "opacity-0"
+          } hover:scale-110`}
+          onMouseOver={() => setSlideContainerHover(true)}
+          onMouseDown={() => slideContainer.current.scrollBy(-1000, 0)}
+        >
+          <span className="material-symbols-outlined text-white text-5xl">
+            chevron_left
+          </span>
         </div>
-        <div className="absolute right-32 top-10 arrow-btns">
+        <div
+          ref={slideContainer}
+          onMouseOver={() => setSlideContainerHover(true)}
+          onMouseLeave={() => setSlideContainerHover(false)}
+          className="slide-container cursor-default active:cursor-grabbing justify-start items-center w-full"
+        >
+          <div className="slide-box bg-[url(https://ld-wp73.template-help.com/wordpress/prod_803/v1/wp-content/uploads/2020/03/slider_picture1.jpg)] w-full h-[670px] bg-cover bg-center bg-no-repeat flex justify-center items-center flex-col text-white active:cursor-grabbing text-center relative z-[-1]">
+            <h3 className="text-xl font-[Times-new-romen] tracking-[10px]">
+              THE WOODS HOTEL
+            </h3>
+            <h1 className="header-title text-5xl px-5 sm:text-8xl">
+              WE'RE A QUIET PLACE TO STAY AT
+            </h1>
+          </div>
+          <div className="slide-box bg-[url(https://ld-wp73.template-help.com/wordpress/prod_803/v1/wp-content/uploads/2020/03/slider_picture2.jpg)] w-full h-[670px] bg-cover bg-center bg-no-repeat flex justify-center items-center flex-col text-white active:cursor-grabbing text-center relative z-[-1]">
+            <h3 className="text-xl font-[Times-new-romen] tracking-[10px]">
+              COMFORTABLE APARTMENTS
+            </h3>
+            <h1 className="header-title text-5xl px-5 sm:text-8xl">
+              ENJOY YOUR STAY WHILE ON A BUSINESS TRIP
+            </h1>
+          </div>
+          <div className="slide-box bg-[url(https://ld-wp73.template-help.com/wordpress/prod_803/v1/wp-content/uploads/2020/03/slider_picture3.jpg)] w-full h-[670px] bg-cover bg-center bg-no-repeat flex justify-center items-center flex-col text-white active:cursor-grabbing text-center relative z-[-1]">
+            <h3 className="text-xl font-[Times-new-romen] tracking-[10px]">
+              ROOMS & SUITES
+            </h3>
+            <h1 className="header-title text-5xl px-5 sm:text-8xl">
+              THE INTRICATE AND ELEGANT, BUT COMFORTABLE
+            </h1>
+          </div>
+        </div>
+        <div
+          className={`absolute duration-300 arrow-right right-10 sm:right-32 top-96 sm:top-64 cursor-pointer ${
+            slideContainerHover ? "opacity-100" : "opacity-0"
+          } hover:scale-110`}
+          onMouseOver={() => setSlideContainerHover(true)}
+          onMouseDown={() => slideContainer.current.scrollBy(1000, 0)}
+        >
           <span className="material-symbols-outlined text-white text-5xl">
             chevron_right
           </span>
