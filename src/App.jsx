fc47169af1fcc67b079e05f63ad13398211a719b
Daniel Ward <02danwar.buss@gmail.com>
2022-12-10 06:35:21 -0600

    deletion period

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import HeaderSection from "./components/HeaderSectionF/HeaderSection";
-import NavigationBar from "./components/NavigationBarF/NavigationBar";
-
 function Body() {
   return (
     <>
-      <div className="select-none">
-        <NavigationBar />
-        <HeaderSection />
-      </div>
-      <div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
+      <div></div>
     </>
   );
 }
