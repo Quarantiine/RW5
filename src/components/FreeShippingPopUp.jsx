aa4b63a870b437d7f770f9b6aa46471afe857481
Daniel Ward <02danwar.buss@gmail.com>
2022-12-13 21:09:23 -0600

    sm change

--- src/components/FreeShippingPopUp.jsx
+++ src/components/FreeShippingPopUp.jsx
@@ -1,37 +1,36 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import gsap from "gsap/dist/gsap";
 
 const FreeShippingPopUp = () => {
-	const [hide, setHide] = useState(false);
-	const popUp = useRef();
+  const [hide, setHide] = useState(true);
 
-	const timedClose = () => {
-		setTimeout(() => setHide(true), 1300);
-		gsap.context(() => {
-			gsap.to(".pop-up-container", {
-				height: 0,
-				overflow: "hidden",
-				opacity: 0,
-				duration: 0.2,
-			});
-		});
-	};
+  const timedClose = () => {
+    gsap.context(() => {
+      gsap.to(".pop-up-container", {
+        height: 0,
+        overflow: "hidden",
+        opacity: 0,
+        duration: 0.2,
+      });
+    });
+    setTimeout(() => setHide(false), 1300);
+  };
 
-	return (
-		<>
-			{!hide && (
-				<div
-					ref={popUp}
-					className="pop-up-container relative bg-gray-100 text-black w-full h-10 flex justify-center items-center"
-				>
-					<p>Free shipping on orders over $35</p>
-					<span onClick={timedClose} className="cursor-pointer material-symbols-outlined absolute right-10">
-						close
-					</span>
-				</div>
-			)}
-		</>
-	);
+  return (
+    <>
+      {hide && (
+        <div className="pop-up-container relative bg-gray-100 text-black w-full h-10 flex justify-center items-center">
+          <p>Free shipping on orders over $35</p>
+          <span
+            onClick={timedClose}
+            className="cursor-pointer material-symbols-outlined absolute right-10"
+          >
+            close
+          </span>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default FreeShippingPopUp;
