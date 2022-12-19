3156097456efa6138fe8f88427353840923d0318
Daniel Ward <02danwar.buss@gmail.com>
2022-12-19 08:18:47 -0600

    sm change

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -5,7 +5,7 @@ const Modal = () => {
 		<>
 			<div className="bg-black w-full h-full opacity-70 fixed z-[200]"></div>
 			<div className="flex fixed z-[201] w-full h-full justify-center items-center">
-				<div className=" w-[850px] h-[550px] bg-white"></div>
+				<div className=" w-[80%] xl:w-[60%] h-[95%] bg-white"></div>
 			</div>
 		</>
 	);
